374b664fb5114dea3e26915a4d05cd42c07c57b6
Luke Gill <bagrad007@gmail.com>
2021-03-15 15:02:11 -0400

    Pod changes

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,11 +1,29 @@
 class PartiesController < ApplicationController
   def index
-      @parties = Party.all
+    @parties = Party.all
   end
 
   def show
-      @party = Party.find(params[:id])
+    @party = Party.find(params[:id])
   end
 
+  def new
+    @party = Party.new
+  end
 
+  def create
+    @party = Party.new(party_params)
+    if @party.save
+      redirect_to party_path(@party)
+    else
+      render :new
+    end
+    # binding.pry
+  end
+
+  private
+
+  def party_params
+    params.require(:party).permit(:name, :budget, :date)
+  end
 end
--- app/models/party.rb
+++ app/models/party.rb
@@ -1,2 +1,5 @@
 class Party < ApplicationRecord
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :budget, presence: true
 end
